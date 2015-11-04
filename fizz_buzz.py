afa0b7cd71abc351e0e392644e54ee352b537806
Charleston Victorino <cv9828@hawaii.edu>
2015-11-03 22:15:26 -1000

    works

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -3,7 +3,15 @@
 def fizz_buzz(number):
     """Return a string representing the number, or 'fizz'/'buzz'/'fizzbuzz'"""
     # TODO - remove 'pass' and write teh codes
-    pass
+   # for n in range (1,20):
+    if number%3==0 and number%5==0:
+        return "fizzbuzz"
+    elif number%5==0:
+        return "buzz"
+    elif number%3==0:
+        return "fizz"
+    else:
+        return str(number)
 
 def main():
     """Print sample output of fizz_buzz function"""
